04c63dde51f4b6100a1a50aa234f0601d92f6751
fmlizarraga <lizarragafranco91@gmail.com>
2024-07-05 20:24:48 -0300

    preparando para deploy

--- vinos/vinos/settings.py
+++ vinos/vinos/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,19 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'http://127.0.0.1:8300/static/'
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
-# Ruta donde se recopilan los archivos estaticos
-STATIC_ROOT = config('STATIC_ROOT')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# URL que usa Django para servir los archivos estaticos
-STATIC_URL = config('STATIC_URL')
-
-# Ubicaciones adicionales para archivos estaticos
-STATICFILES_DIRS = [
-    config('STATICFILES_EXTRA'),
-]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
